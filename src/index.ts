f130ddc111c5a9b820d527ad8cf9c78aa24528f3
alexanderjaramillo <alexander.jaramillo@ceiba.com.co>
2020-11-20 12:35:56 +0000

    ejemplo clase

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hola");
+class Organizador {
+  agregarNodo(i: number, j: number) {
+    console.log(`agregando (${i}, ${j})`);
+  }
+}
+
+const organizador = new Organizador();
+organizador.agregarNodo(1, 3);
